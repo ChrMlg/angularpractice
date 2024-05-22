13f5d61dcdb00c1b44524da4f7952e998c29a0b3
ChrMlg <00110219@uca.edu.sv>
2024-05-21 18:00:45 -0600

    corrected standalone app to no standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-}
+  title = 'Tour of Heroes';}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { MockHeroServiceService } from '../mock-hero-service.service';
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent implements OnInit {
-   mockData: any[] = [];
-
-  constructor(private mockHeroService: MockHeroServiceService) { }
-
-  ngOnInit() {
-    this.mockData = this.mockHeroService.getMockData();
-  }
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
